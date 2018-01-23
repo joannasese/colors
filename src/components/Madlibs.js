cab087a2116fae1a352b037a5a1aa90ad6ee114f
Joanna <jmgsese@gmail.com>
2018-01-23 01:46:01 -0600

    Pseudocode madlibs

---
+++ src/components/Madlibs.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { verbs } from './data';
+
+Form to submit noun. (persist in data array, ditto for all below)
+Form to submit adjective.
+Form to submit verb.
+Submit button.
+
+On submit, generate madlib.
--- src/components/data.js
+++ src/components/data.js
@@ -4,6 +4,8 @@ let videos =
   "https://www.youtube.com/embed/InCUx_Z3AYo?rel=0",
   "https://www.youtube.com/embed/Vf_4juIwad0?rel=0",
   "https://www.youtube.com/embed/fINwkO3IpX8?rel=0"
-]
+];
 
-module.exports = {videos}
+let verbs = [];
+
+module.exports = {videos, verbs}
