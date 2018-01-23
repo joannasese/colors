0a7797432837f8ff868444e5b834574794f75d15
Joanna <jmgsese@gmail.com>
2018-01-22 18:26:57 -0600

    Add video to data and put in placeholder button

--- src/components/LittleJoy.js
+++ src/components/LittleJoy.js
@@ -4,8 +4,12 @@ import { videos } from './data';
 const LittleJoy = () => {
   return (
     <div>
-      <iframe title="random-video" width="560" height="315" src={videos[Math.floor(Math.random()*videos.length)]} frameborder="0"></iframe>
+      <iframe title="random-video" width="560" height="315" src={videos[Math.floor(Math.random()*videos.length)]}></iframe>
+      <div>
+        <button>I like this</button>
+      </div>
     </div>
+
   )
 }
 
--- src/components/data.js
+++ src/components/data.js
@@ -2,7 +2,8 @@ let videos =
 [
   "https://www.youtube.com/embed/fLoT0rqkMYI?rel=0",
   "https://www.youtube.com/embed/InCUx_Z3AYo?rel=0",
-  "https://www.youtube.com/embed/Vf_4juIwad0?rel=0"
+  "https://www.youtube.com/embed/Vf_4juIwad0?rel=0",
+  "https://www.youtube.com/embed/fINwkO3IpX8?rel=0"
 ]
 
 module.exports = {videos}
