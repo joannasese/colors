b54ba237ea8f11a7f65a569d1737a1f517346825
Joanna <jmgsese@gmail.com>
2018-01-23 02:44:30 -0600

    Add madlib verb form

--- src/components/Madlibs.js
+++ src/components/Madlibs.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import { verbs } from './data';
 
-const Madlibs = () => {
-  return (
-    <div>
-    <p>Form to submit noun. (persist in data array, ditto for all below)</p>
-    <p>Form to submit adjective.</p>
-    <p>Form to submit verb.</p>
-    <p>Submit button.</p>
+class Madlibs extends React.Component {
+  constructor(props){
+    super(props)
 
-    <p>On submit, generate madlib.</p>
-    </div>
-  )
+    this.state = {
+      verb: '',
+    }
+  }
+
+  handleChange(event) {
+    this.setState({
+      verb: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_VERB',
+      verb: this.state,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <p>
+            <label>Submit a verb:</label>
+            <input type="text" value={this.state.verb} onChange={(event) => this.handleChange(event)} />
+          </p>
+          <input type="submit" />
+        </form>
+        <p>Form to submit noun. (persist in data array, ditto for all below?)</p>
+        <p>Form to submit adjective.</p>
+        <p>Form to submit verb.</p>
+        <p>Submit button.</p>
+
+        <p>On submit, generate madlib.</p>
+      </div>
+    )
+  }
 }
 
 export default Madlibs;
