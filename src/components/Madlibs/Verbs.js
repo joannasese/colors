6649e6d7be3704cdd811c49b4e1c07ffa60ec742
Joanna <jmgsese@gmail.com>
2018-01-23 23:39:51 -0600

    Simply sentence format

--- src/components/Madlibs/Verbs.js
+++ src/components/Madlibs/Verbs.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+const Verbs = ({verbs}) =>
+  <div>
+   {verbs.map(verb =>
+     <h2>I want to <strong>{verb}</strong> it.</h2>
+   )}
+  </div>
 
-
-export class Verbs extends Component {
-  render() {
-    // const verbs = this.props.store.getState().verbs.map((verb, index) => {
-    //   return <Verb text={verb.text} key={index} />
-    // });
-    console.log(this.props.verbs)
-    let verbs = this.props.verbs.map((verb, index) =>
-      <li key={index}>
-        <h1>I wanted to <strong>{verb}</strong> melons.</h1>
-      </li>
-    );
-
-    return(
-      <ul>
-        {verbs}
-      </ul>
-    );
-  }
-};
+// export class Verbs extends Component {
+//   render() {
+//     console.log(this.props.verbs)
+//     let verbs = this.props.verbs.map((verb, index) =>
+//       <li key={index}>
+//         <h1>I wanted to <strong>{verb}</strong> melons.</h1>
+//       </li>
+//     );
+//
+//     return(
+//       <ul>
+//         {verbs}
+//       </ul>
+//     );
+//   }
+// };
 
 function mapStateToProps (state){
   return { verbs: state.verbs };
