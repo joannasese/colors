f41c7cc42e2c348e77a6f5784eb002815f6c096d
Joanna <jmgsese@gmail.com>
2018-01-23 22:41:19 -0600

    Remove extra code

--- src/components/Madlibs/Madlibs.js
+++ src/components/Madlibs/Madlibs.js
@@ -19,19 +19,13 @@ class Madlibs extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    // {verbs}.addVerb(this.state)
     this.props.addVerb(this.state);
-    console.log({ConnectedVerbs})
-    // this.setState({
-    //   verb: '',
-    // });
   }
 
   render() {
     return (
       <div>
         <p>Number of verbs in Verbs array: {Verbs.length}</p>
-
         <p>It's madlib time! Provide a noun, an adjective and a verb. We'll give you a wacky phrase in return!</p>
         <form onSubmit={(event) => this.handleSubmit(event)}>
           <p>
@@ -46,7 +40,6 @@ class Madlibs extends React.Component {
         </form>
         <p>The ADJ NOUN likes to <strong>{this.state.verb}</strong> while drinking his coffee.</p>
       </div>
-
     )
   }
 }
