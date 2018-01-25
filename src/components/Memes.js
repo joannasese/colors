7af0e536e72f3e25d574e5245ecc3602ac54bb2c
Joanna <jmgsese@gmail.com>
2018-01-25 14:56:06 -0600

    Add fetchData method to meme sandbox

--- src/components/Memes.js
+++ src/components/Memes.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-//stateless component
-const Memes = () => {
-  return (
-    <img src="https://i03.hsncdn.com/is/image/HomeShoppingNetwork/prodfull/colorbox-pigment-inkpads-d-2008052313222943~4405288w.jpg" alt="pigments" />
-  )
+class Memes extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+          memes: [],
+          hasErrored: false,
+          isLoading: false
+        };
+    }
+
+    fetchData(url) {
+      this.setState({ isLoading: true });
+
+      fetch(url)
+          .then((response) => {
+              if (!response.ok) {
+                  throw Error(response.statusText);
+              }
+
+              this.setState({ isLoading: false });
+
+              return response;
+          })
+          .then((response) => response.json())
+          .then((memes) => this.setState({ memes }))
+          .catch(() => this.setState({ hasErrored: true }));
+    }
+
+    render() {
+        if (this.state.hasErrored) {
+            return <p>Sorry! There was an error loading the memes</p>;
+        }
+
+        if (this.state.isLoading) {
+            return <p>Loading…</p>;
+        }
+
+        return (
+            <ul>
+                {this.state.memes.map((meme) => (
+                    <li key={meme.id}>
+                        {meme.label}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
 }
 
 export default Memes;
